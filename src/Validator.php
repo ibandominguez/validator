5987896a41e72be203f5aecfc12aca5ba6c3517d
ibandominguez <ibandominguez@live.com>
2015-05-13 13:45:59 +0100

    required validation

--- src/Validator.php
+++ src/Validator.php
@@ -4,4 +4,145 @@ namespace EasyCoding;
 
 class Validator {
 
+  /**
+   * inputs
+   *
+   * @var
+   */
+  protected $inputs = array();
+
+  /**
+   * rules
+   *
+   * @var
+   */
+  protected $rules = array();
+
+  /**
+   * messages
+   *
+   * @var
+   */
+  protected $messages = array();
+
+  /**
+   * errors
+   *
+   * @var
+   */
+  protected $errors = array();
+
+  /**
+   * passes
+   *
+   * @var
+   */
+  protected $passes = true;
+
+  /**
+   * PUBLIC METHODS
+   */
+
+  /**
+   * __construct
+   *
+   * @param array inputs
+   * @param array rules
+   * @param array messages
+   * @return void
+   */
+  public function __construct(array $inputs = array(), array $rules = array(), array $messages = array())
+  {
+    // let´s set the data
+    // so we can make use of it
+    // through the methods
+    $this->inputs = $inputs;
+    $this->rules = $rules;
+    $this->messages = $messages;
+
+    // let´s trigger the validate method
+    $this->validate();
+  }
+
+  /**
+   * passes
+   *
+   * @return boolean
+   */
+  public function passes()
+  {
+    return $this->passes;
+  }
+
+  /**
+   * errors
+   *
+   * @return array
+   */
+  public function getErrors()
+  {
+    return $this->errors;
+  }
+
+  /**
+   * PROTECTED METHODS
+   */
+
+  /**
+   * validate
+   *
+   * takes care of global validation
+   *
+   *
+   * @return void
+   */
+  protected function validate()
+  {
+    // lets get the ruleset
+    // where $v is something like 'required|email|size:8'
+    foreach ($this->rules as $key => $value) :
+
+      $ruleset = explode('|', $value);
+
+      // lets get the params from the ruleset
+      // where $v is something like 'size:8'
+      foreach ($ruleset as $rule) :
+        $pos = strpos($rule, ':');
+        $params = '';
+
+        if ($pos !== false) {
+          $params = substr($rule, $pos + 1);
+          $rule = substr($rule, 0, $pos);
+        }
+
+        // now we have rule and params
+        // so we need to find out if
+        // there's a method to validate
+        $methodName = 'validate' . ucfirst($rule);
+
+        if (method_exists($this, $methodName)) {
+          if (!$this->$methodName($key, $rule, $params)) {
+            $this->passes = false;
+            $this->errors[$key] = isset($this->messages[$key]) ? $this->messages[$key] : $key . " is " . $rule;
+          }
+        } else {
+          throw new \BadMethodCallException($methodName . ' method does not exists');
+        }
+
+      endforeach;
+
+    endforeach;
+  }
+
+  /**
+   * validate required
+   *
+   * @param string
+   * @param string
+   * @return boolean
+   */
+  protected function validateRequired($key, $validation, $params) {
+    return !empty($this->inputs[$key]);
+  }
+
 }
--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -2,4 +2,77 @@
 
 class ValidatorTest extends PHPUnit_Framework_TestCase {
 
+  /**
+   * test instance
+   *
+   * @test
+   */
+  public function testInstance()
+  {
+    $this->assertInstanceOf('\EasyCoding\Validator', new \EasyCoding\Validator);
+  }
+
+  /**
+   * testPassesByDefault
+   *
+   * @test
+   */
+  public function testPassesByDefault()
+  {
+    $v = new \EasyCoding\Validator();
+
+    $this->assertTrue($v->passes(), 'Failed asserting that validation passes by default');
+  }
+
+  /**
+   * testRequiredMethodPasses
+   *
+   * @test
+   */
+  public function testRequiredMethodPasses()
+  {
+    $inputs = array('name' => 'hello');
+    $rules = array('name' => 'required');
+
+    $v = new \EasyCoding\Validator($inputs, $rules);
+
+    $this->assertTrue($v->passes(), 'Failed asserting that required validation fails if empty value');
+    $this->assertCount(0, $v->getErrors(), 'Failed asserting that they were no errors');
+  }
+
+  /**
+   * testRequiredMethodFails
+   *
+   * @test
+   */
+  public function testRequiredMethodFails()
+  {
+    $inputs = array('name' => '');
+    $rules = array('name' => 'required');
+
+    $v = new \EasyCoding\Validator($inputs, $rules);
+
+    $this->assertFalse($v->passes(), 'Failed asserting that required validation fails if empty value');
+    $this->assertCount(1, $v->getErrors(), 'Failed asserting that errors length is one');
+    $this->assertArrayHasKey('name', $v->getErrors(), 'Failed asserting that errors contains name');
+  }
+
+  /**
+   * testRequiredCustomMessage
+   *
+   * @test
+   */
+  public function testRequiredCustomMessage()
+  {
+    $inputs = array('name' => '');
+    $rules = array('name' => 'required');
+    $messages = array('name' => 'el nombre es obligatorio');
+
+    $v = new \EasyCoding\Validator($inputs, $rules, $messages);
+
+    $errors = $v->getErrors();
+
+    $this->assertEquals($messages['name'], $errors['name'], 'Failed asserting that custom message was passed in');
+  }
+
 }
