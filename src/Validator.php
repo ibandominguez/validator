abf4781f352e69a3dd15f73d3de745a82e50a027
ibandominguez <ibandominguez@live.com>
2015-11-02 14:02:54 +0000

    corrected indentation

--- src/Validator.php
+++ src/Validator.php
@@ -2,184 +2,189 @@
 
 namespace EasyCoding;
 
-class Validator {
+class Validator
+{
 
-  /**
-   * inputs
-   *
-   * @var
-   */
-  protected $inputs = array();
+    /**
+     * inputs
+     *
+     * @var
+     */
+    protected $inputs = array();
 
-  /**
-   * rules
-   *
-   * @var
-   */
-  protected $rules = array();
+    /**
+     * rules
+     *
+     * @var
+     */
+    protected $rules = array();
 
-  /**
-   * messages
-   *
-   * @var
-   */
-  protected $messages = array();
+    /**
+     * messages
+     *
+     * @var
+     */
+    protected $messages = array();
 
-  /**
-   * errors
-   *
-   * @var
-   */
-  protected $errors = array();
+    /**
+     * errors
+     *
+     * @var
+     */
+    protected $errors = array();
 
-  /**
-   * passes
-   *
-   * @var
-   */
-  protected $passes = true;
+    /**
+     * passes
+     *
+     * @var
+     */
+    protected $passes = true;
 
-  /**
-   * PUBLIC METHODS
-   */
+    /**
+     * PUBLIC METHODS
+     */
 
-  /**
-   * __construct
-   *
-   * @param array inputs
-   * @param array rules
-   * @param array messages
-   * @return void
-   */
-  public function __construct(array $inputs = array(), array $rules = array(), array $messages = array())
-  {
-    // let´s set the data
-    // so we can make use of it
-    // through the methods
-    $this->inputs = $inputs;
-    $this->rules = $rules;
-    $this->messages = $messages;
+    /**
+     * __construct
+     *
+     * @param array inputs
+     * @param array rules
+     * @param array messages
+     * @return void
+     */
+    public function __construct(array $inputs = array(), array $rules = array(), array $messages = array())
+    {
+        // let´s set the data
+        // so we can make use of it
+        // through the methods
+        $this->inputs = $inputs;
+        $this->rules = $rules;
+        $this->messages = $messages;
 
-    // let´s trigger the validate method
-    $this->validate();
-  }
+        // let´s trigger the validate method
+        $this->validate();
+    }
 
-  /**
-   * passes
-   *
-   * @return boolean
-   */
-  public function passes()
-  {
-    return $this->passes;
-  }
+    /**
+     * passes
+     *
+     * @return boolean
+     */
+    public function passes()
+    {
+        return $this->passes;
+    }
 
-  /**
-   * errors
-   *
-   * @return array
-   */
-  public function getErrors()
-  {
-    return $this->errors;
-  }
+    /**
+     * errors
+     *
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 
-  /**
-   * PROTECTED METHODS
-   */
+    /**
+     * PROTECTED METHODS
+     */
 
-  /**
-   * validate
-   *
-   * takes care of global validation
-   *
-   *
-   * @return void
-   */
-  protected function validate()
-  {
-    // lets get the ruleset
-    // where $v is something like 'required|email|size:8'
-    foreach ($this->rules as $key => $value) :
+    /**
+     * validate
+     *
+     * takes care of global validation
+     *
+     *
+     * @return void
+     */
+    protected function validate()
+    {
+        // lets get the ruleset
+        // where $v is something like 'required|email|size:8'
+        foreach ($this->rules as $key => $value) :
 
-      $ruleset = explode('|', $value);
+            $ruleset = explode('|', $value);
 
-      // lets get the params from the ruleset
-      // where $v is something like 'size:8'
-      foreach ($ruleset as $rule) :
-        $pos = strpos($rule, ':');
-        $params = '';
+            // lets get the params from the ruleset
+            // where $v is something like 'size:8'
+            foreach ($ruleset as $rule) :
+                $pos = strpos($rule, ':');
+                $params = '';
 
-        if ($pos !== false) {
-          $params = substr($rule, $pos + 1);
-          $rule = substr($rule, 0, $pos);
-        }
+                if ($pos !== false) {
+                    $params = substr($rule, $pos + 1);
+                    $rule = substr($rule, 0, $pos);
+                }
 
-        // now we have rule and params
-        // so we need to find out if
-        // there's a method to validate
-        $methodName = 'validate' . ucfirst($rule);
+                // now we have rule and params
+                // so we need to find out if
+                // there's a method to validate
+                $methodName = 'validate' . ucfirst($rule);
 
-        if (method_exists($this, $methodName)) {
-          if (!$this->$methodName($key, $rule, $params)) {
-            $this->passes = false;
-            $this->errors[$key] = isset($this->messages[$key]) ? $this->messages[$key] : $key . ", rule: " . $rule;
-          }
-        } else {
-          throw new \BadMethodCallException($methodName . ' method does not exists');
-        }
+                if (method_exists($this, $methodName)) {
+                    if (!$this->$methodName($key, $rule, $params)) {
+                        $this->passes = false;
+                        $this->errors[$key] = isset($this->messages[$key]) ? $this->messages[$key] : $key . ", rule: " . $rule;
+                    }
+                } else {
+                    throw new \BadMethodCallException($methodName . ' method does not exists');
+                }
 
-      endforeach;
+            endforeach;
 
-    endforeach;
-  }
+        endforeach;
+    }
 
-  /**
-   * validate required
-   *
-   * @param string
-   * @param string
-   * @param string
-   * @return boolean
-   */
-  protected function validateRequired($key, $validation, $params) {
-    return !empty($this->inputs[$key]);
-  }
+    /**
+     * validate required
+     *
+     * @param string
+     * @param string
+     * @param string
+     * @return boolean
+     */
+    protected function validateRequired($key, $validation, $params)
+    {
+        return !empty($this->inputs[$key]);
+    }
 
-  /**
-   * validate array
-   *
-   * @param string
-   * @param string
-   * @param string
-   * @return boolean
-   */
-  protected function validateArray($key, $validation, $params) {
-    return is_array($this->inputs[$key]);
-  }
+    /**
+     * validate array
+     *
+     * @param string
+     * @param string
+     * @param string
+     * @return boolean
+     */
+    protected function validateArray($key, $validation, $params)
+    {
+        return is_array($this->inputs[$key]);
+    }
 
-  /**
-   * validate email
-   *
-   * @param string
-   * @param string
-   * @param string
-   * @return boolean
-   */
-  protected function validateEmail($key, $validation, $params) {
-    return filter_var($this->inputs[$key], FILTER_VALIDATE_EMAIL);
-  }
+    /**
+     * validate email
+     *
+     * @param string
+     * @param string
+     * @param string
+     * @return boolean
+     */
+    protected function validateEmail($key, $validation, $params)
+    {
+        return filter_var($this->inputs[$key], FILTER_VALIDATE_EMAIL);
+    }
 
-  /**
-   * validate numeric
-   *
-   * @param string
-   * @param string
-   * @param string
-   * @return boolean
-   */
-  protected function validateNumeric($key, $validation, $params) {
-    return is_numeric($this->inputs[$key]);
-  }
+    /**
+     * validate numeric
+     *
+     * @param string
+     * @param string
+     * @param string
+     * @return boolean
+     */
+    protected function validateNumeric($key, $validation, $params)
+    {
+        return is_numeric($this->inputs[$key]);
+    }
 
 }
--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -1,189 +1,190 @@
 <?php
 
-class ValidatorTest extends PHPUnit_Framework_TestCase {
+class ValidatorTest extends PHPUnit_Framework_TestCase
+{
 
-  /**
-   * test instance
-   *
-   * @test
-   */
-  public function testInstance()
-  {
-    $this->assertInstanceOf('\EasyCoding\Validator', new \EasyCoding\Validator);
-  }
+    /**
+     * test instance
+     *
+     * @test
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf('\EasyCoding\Validator', new \EasyCoding\Validator);
+    }
 
-  /**
-   * testPassesByDefault
-   *
-   * @test
-   */
-  public function testPassesByDefault()
-  {
-    $v = new \EasyCoding\Validator();
+    /**
+     * testPassesByDefault
+     *
+     * @test
+     */
+    public function testPassesByDefault()
+    {
+        $v = new \EasyCoding\Validator();
 
-    $this->assertTrue($v->passes(), 'Failed asserting that validation passes by default');
-  }
+        $this->assertTrue($v->passes(), 'Failed asserting that validation passes by default');
+    }
 
-  /**
-   * testRequiredMethod
-   *
-   * @test
-   */
-  public function testRequiredMethod()
-  {
-    $inputs = array('name' => 'hello');
-    $rules = array('name' => 'required');
+    /**
+     * testRequiredMethod
+     *
+     * @test
+     */
+    public function testRequiredMethod()
+    {
+        $inputs = array('name' => 'hello');
+        $rules = array('name' => 'required');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertTrue($v->passes(), 'Failed asserting that required validation fails if empty value');
-    $this->assertCount(0, $v->getErrors(), 'Failed asserting that they were no errors');
-  }
+        $this->assertTrue($v->passes(), 'Failed asserting that required validation fails if empty value');
+        $this->assertCount(0, $v->getErrors(), 'Failed asserting that they were no errors');
+    }
 
-  /**
-   * testRequiredMethodFails
-   *
-   * @test
-   */
-  public function testRequiredMethodFails()
-  {
-    $inputs = array('name' => '');
-    $rules = array('name' => 'required');
+    /**
+     * testRequiredMethodFails
+     *
+     * @test
+     */
+    public function testRequiredMethodFails()
+    {
+        $inputs = array('name' => '');
+        $rules = array('name' => 'required');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertFalse($v->passes(), 'Failed asserting that required validation fails if empty value');
-    $this->assertCount(1, $v->getErrors(), 'Failed asserting that errors length is one');
-    $this->assertArrayHasKey('name', $v->getErrors(), 'Failed asserting that errors contains name');
-  }
+        $this->assertFalse($v->passes(), 'Failed asserting that required validation fails if empty value');
+        $this->assertCount(1, $v->getErrors(), 'Failed asserting that errors length is one');
+        $this->assertArrayHasKey('name', $v->getErrors(), 'Failed asserting that errors contains name');
+    }
 
-  /**
-   * testExpectedErrorMessage
-   *
-   * @test
-   */
-  public function testExpectedErrorMessage()
-  {
-    $inputs = array('name' => '');
-    $rules = array('name' => 'required');
+    /**
+     * testExpectedErrorMessage
+     *
+     * @test
+     */
+    public function testExpectedErrorMessage()
+    {
+        $inputs = array('name' => '');
+        $rules = array('name' => 'required');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $errors = $v->getErrors();
+        $errors = $v->getErrors();
 
-    $this->assertEquals('name, rule: required', $errors['name'], 'Failed asserting that message is outputed correctly');
-  }
+        $this->assertEquals('name, rule: required', $errors['name'], 'Failed asserting that message is outputed correctly');
+    }
 
-  /**
-   * testRequiredCustomMessage
-   *
-   * @test
-   */
-  public function testRequiredCustomMessage()
-  {
-    $inputs = array('name' => '');
-    $rules = array('name' => 'required');
-    $messages = array('name' => 'el nombre es obligatorio');
+    /**
+     * testRequiredCustomMessage
+     *
+     * @test
+     */
+    public function testRequiredCustomMessage()
+    {
+        $inputs = array('name' => '');
+        $rules = array('name' => 'required');
+        $messages = array('name' => 'el nombre es obligatorio');
 
-    $v = new \EasyCoding\Validator($inputs, $rules, $messages);
+        $v = new \EasyCoding\Validator($inputs, $rules, $messages);
 
-    $errors = $v->getErrors();
+        $errors = $v->getErrors();
 
-    $this->assertEquals($messages['name'], $errors['name'], 'Failed asserting that custom message was passed in');
-  }
+        $this->assertEquals($messages['name'], $errors['name'], 'Failed asserting that custom message was passed in');
+    }
 
-  /**
-   * testArrayMethod
-   *
-   * @test
-   */
-  public function testArrayMethod()
-  {
-    $inputs = array('array' => array());
-    $rules = array('array' => 'array');
+    /**
+     * testArrayMethod
+     *
+     * @test
+     */
+    public function testArrayMethod()
+    {
+        $inputs = array('array' => array());
+        $rules = array('array' => 'array');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertTrue($v->passes());
+        $this->assertTrue($v->passes());
 
-    $this->assertEquals(array(), $inputs['array'], 'Failed asserting that array method works');
-  }
+        $this->assertEquals(array(), $inputs['array'], 'Failed asserting that array method works');
+    }
 
-  /**
-   * testArrayMethodFails
-   *
-   * @test
-   */
-  public function testArrayMethodFails()
-  {
-    $inputs = array('array' => '');
-    $rules = array('array' => 'array');
+    /**
+     * testArrayMethodFails
+     *
+     * @test
+     */
+    public function testArrayMethodFails()
+    {
+        $inputs = array('array' => '');
+        $rules = array('array' => 'array');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertFalse($v->passes());
+        $this->assertFalse($v->passes());
 
-    $this->assertNotEquals(array(), $inputs['array'], 'Failed asserting that array method works');
-  }
+        $this->assertNotEquals(array(), $inputs['array'], 'Failed asserting that array method works');
+    }
 
-  /**
-   * testEmailMethod
-   *
-   * @test
-   */
-  public function testEmailMethod()
-  {
-    $inputs = array('email' => 'your@email.com');
-    $rules = array('email' => 'email');
+    /**
+     * testEmailMethod
+     *
+     * @test
+     */
+    public function testEmailMethod()
+    {
+        $inputs = array('email' => 'your@email.com');
+        $rules = array('email' => 'email');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertTrue($v->passes());
-  }
+        $this->assertTrue($v->passes());
+    }
 
-  /**
-   * testEmailMethodFails
-   *
-   * @test
-   */
-  public function testEmailMethodFails()
-  {
-    $inputs = array('email' => 'your');
-    $rules = array('email' => 'email');
+    /**
+     * testEmailMethodFails
+     *
+     * @test
+     */
+    public function testEmailMethodFails()
+    {
+        $inputs = array('email' => 'your');
+        $rules = array('email' => 'email');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertFalse($v->passes());
-  }
+        $this->assertFalse($v->passes());
+    }
 
-  /**
-   * testNumericMethod
-   *
-   * @test
-   */
-  public function testNumericMethod()
-  {
-    $inputs = array('number' => '1', 'othernumber' => 2);
-    $rules = array('number' => 'numeric', 'othernumber' => 'numeric');
+    /**
+     * testNumericMethod
+     *
+     * @test
+     */
+    public function testNumericMethod()
+    {
+        $inputs = array('number' => '1', 'othernumber' => 2);
+        $rules = array('number' => 'numeric', 'othernumber' => 'numeric');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertTrue($v->passes());
-  }
+        $this->assertTrue($v->passes());
+    }
 
-  /**
-   * testNumericMethodFails
-   *
-   * @test
-   */
-  public function testNumericMethodFails()
-  {
-    $inputs = array('number' => 'nonumber');
-    $rules = array('number' => 'numeric');
+    /**
+     * testNumericMethodFails
+     *
+     * @test
+     */
+    public function testNumericMethodFails()
+    {
+        $inputs = array('number' => 'nonumber');
+        $rules = array('number' => 'numeric');
 
-    $v = new \EasyCoding\Validator($inputs, $rules);
+        $v = new \EasyCoding\Validator($inputs, $rules);
 
-    $this->assertFalse($v->passes());
-  }
+        $this->assertFalse($v->passes());
+    }
 
 }
